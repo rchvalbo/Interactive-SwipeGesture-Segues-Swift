9afc8363370ae0e2eb880fd23c5b9bab6f63455f
Roman <rchvalbo@knights.ucf.edu>
2017-09-04 11:28:50 -0400

    InteractiveSwipeGestureProject

---
+++ SwipeGestureRec/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  SwipeGestureRec
+//
+//  Created by Feedr MacBook on 8/17/17.
+//  Copyright © 2017 SapienSolutions, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func handlePan(recognizer:UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: self.view)
+        if let view = recognizer.view {
+            view.center = CGPoint(x:view.center.x + translation.x,
+                                  y:view.center.y + translation.y)
+        }
+        recognizer.setTranslation(CGPoint.zero, in: self.view)
+    }
+
+}
+
